74c50a4add2708c2c7982e747d15d34f9eba4dc8
09ubberboy90 <33180714+09ubberboy90@users.noreply.github.com>
2019-06-06 09:45:25 +0200

    Solved potentiometer and usb WIP pwm

--- Roghec_Main/header/pwm.c
+++ Roghec_Main/header/pwm.c
@@ -8,38 +8,60 @@
 
 #include "pwm.h"
 
-
-void PWMinit()
-{
-    PR2 = 255;                                  // Fréquence 2900Hz
-    CCP1CON=0b00001100;                         // PWM mode, précision 8 bits
-    CCP2CON=0b00001100;                         // PWM mode, précision 8 bits
-    TRISCbits.RC2=0;                            //RC2 in output
-    TRISCbits.RC1=0;                            //RC1 in output
-    LATCbits.LATC1 = 0;                         // Use LAT to write or use POR to read only
-    LATCbits.LATC2 = 0;
-    TMR2IF=0;                                   // Flag initialize at 0
-    T2CON=0b00000011;                           // Prescaler 16
-    T2CONbits.TMR2ON=1;                         // Enable Timer2
+void PWMinit(unsigned char frequency,uint8_t channel){
+    if(channel == 1){
+        TRISC2 = 0;
+        CCP1M3 = 1;
+        CCP1M2 = 1;
+    }
+    if(channel == 2){
+        TRISC1 = 0; 
+        CCP2M3 = 1;
+        CCP2M2 = 1;
+    }       //  PWM mode ( CCP1M3:CCP1M0 = 1100)
+    /* Timer2 configuration */
+    while (TMR2ON !=1)
+    {
+        PR2 = 48000000/(4*frequency*16)-1           //  configure timer2 period
+        T2CON = 0x02;           //  Set prescalar 16   
+        TMR2ON = 1;             //  timer2 on
+    }
 }
 
-uint16_t Forward_Dir(uint8_t value)
-{
-    // To implement actual forward dir
-    uint16_t Value_out = (value * PR2) / 100;
-    CCPR1L = Value_out;
-    CCPR2L = Value_out + 10;
-    return Value_out;
-
+void PWM1_setDC(unsigned int dutycycle,uint8_t channel){
+    
+    CCPR1L = dutycycle>>2;  //  PWM duty cycle - first 8-bits (MSb)
+    CCP1CON &= 0xCF;        //  5,4 bits zeroed (DC1B1:DC1B0 = 00)
+    CCP1CON |= ((dutycycle<<4)&0x30);  //  PWM duty cycle - last 2-bits (LSb) in CCP1CON 5,4 bits    
+    if(channel == 1){
+        CCPR1L = dutycycle>>2;  //  PWM duty cycle - first 8-bits (MSb)
+        CCP1CON &= 0xCF;        //  5,4 bits zeroed (DC1B1:DC1B0 = 00)
+        CCP1CON |= ((dutycycle<<4)&0x30);  //  PWM duty cycle - last 2-bits (LSb) in CCP1CON 5,4 bits    
+    }
+    if(channel == 2){
+        CCPR2L = dutycycle>>2;  //  PWM duty cycle - first 8-bits (MSb)
+        CCP2CON &= 0xCF;        //  5,4 bits zeroed (DC1B1:DC1B0 = 00)
+        CCP2CON |= ((dutycycle<<4)&0x30);  //  PWM duty cycle - last 2-bits (LSb) in CCP1CON 5,4 bits    
+    }
 }
 
-uint16_t Reverse_Dir(uint8_t value)
+void PWM_Start(unsigned char frequency,unsigned int dutycycle,uint8_t channel)
+ {
+    /*open PWM at 2KHz*/ 
+    PWM1_Init(frequency,channel);
+    
+    /*set duty cycle 0 - 1023 range */
+    PWM1_setDC(dutycycle,channel);
+ }
+
+uint16_t Right_Motor(uint8_t value)
 {
-    // To implement actual reverse dir
-    uint16_t Value_out = (value * PR2) / 100;
-    CCPR1L = Value_out;
-    CCPR2L = Value_out + 10;
-    return Value_out;
+    PWM_Start(0x4E20,(value*1024)/100,1);
+}
+
+uint16_t Left_Motor(uint8_t value)
+{
+    PWM_Start(0x4E20,(value*1024)/100,1);
 }
 
 uint16_t hex2int(char *hex) {
